2bde7a015ecf0f5a8b38322a73d1565168076958
MRodzDirect - Original <mabarca1@utpl.edu.ec>
2024-04-24 17:24:07 -0500

    Another commit

--- EjerciciosClase/src/practicaTaller2/Conductor.java
+++ EjerciciosClase/src/practicaTaller2/Conductor.java
@@ -10,42 +10,75 @@ public class Conductor {
 
     private String name;
     private String licenceType;
-    private String age; // anio que nacio
-    private String licenceType;
-    
-
+    private int age; // anio que nacio
+    private String licenceYear;
+    private String vehicle;
+    private int multas;
     public Scanner in;
     
     
-    public Conductor(){
+    public Conductor(){ // Constructor por default
     
         in = new Scanner(System.in);
 }
    
-    public Conductor(String color, int speed, int traction){
-        this.color=color;
-        this.speed=speed;
-        this.traction=traction;
-        in = new Scanner(System.in);
-        System.out.printf("Color del coche: %s , velocidad: %d km/h , traccion: %d\n",color,speed,traction);
-    }
-    
 
-    public void setColor(String color) {
-        this.color=color; 
+        public String getName() {
+        return name;
     }
+
+    public String getLicenceType() {
+        return licenceType;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLicenceYear() {
+        return licenceYear;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    // Setter Methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLicenceType(String licenceType) {
+        this.licenceType = licenceType;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setLicenceYear(String licenceYear) {
+        this.licenceYear = licenceYear;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
     
-    public void validarVehiculo(int llantas){
-        switch (llantas) {
-            case 1: this.name="moto";
-            this.licenceType="B";
-            case 2: this.name="Tricimoto";
-            case 3: this.name="Carro/camioneta";
-            case 4:this.name="Camion/trailer";
+    public void validarEdad(){
+        if (age<18) {
+            System.out.println("Usted sera multado, es menor de edad");  
+            multas++;
+        }
+    }
+    public void validarVehiculo(){
+        switch (vehiculo) {
+            case "moto":
+                this.licenceType="Tipo A";
                 break;
-            default: System.out.println("Su vehiculo no tiene suficientes llantas");
+            default:
                 throw new AssertionError();
-    
-}
+        }
     }
-}
+    }
+
--- EjerciciosClase/src/practicaTaller2/Main.java
+++ EjerciciosClase/src/practicaTaller2/Main.java
@@ -5,9 +5,16 @@ package practicaTaller2;
 public class Main {
     
     public static void main(String[] args) {
-    Conductor chofer1 = new Conductor(); //name, 
+    Conductor chofer1 = new Conductor(); //
+    System.out.println("Ingrese su nombre, tipo de licencia, edad y año de expedicion de licencia");
+            
     String name = chofer1.in.nextLine();
-    String 
+    String licenceType = chofer1.in.nextLine();
+    String age = chofer1.in.nextLine();
+    String licenceYear = chofer1.in.nextLine();
+    System.out.println("Ingrese el vehiculo que usted tiene y le comunicaremos si esta sano de multas o debe pagar alguna.");
+    String vehicle = chofer1.in.nextLine();
+    
     }
  
     
