30d061a4a1b8988b4f0d5377dea249c3588b7303
MRodz <mabarca1@utpl.edu.ec>
2024-04-24 10:24:39 -0500

    Pocos ejercicios add

---
+++ EjerciciosClase/src/practica1/Transporte.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1;
+
+/**
+ *
+ * @author utpl
+ */
+public class Transporte {
+    public String tipo;
+    public String name;
+    public String date;
+    
+    public void mover(){
+        
+    }
+    public void cargar(){
+        
+    }
+}
---
+++ EjerciciosClase/src/practica1/Vehiculo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1;
+
+/**
+ *
+ * @author utpl
+ */
+public class Vehiculo {
+    
+    public String color;
+    public String speed;
+    public String traction;
+    
+    public void marchar(){
+        
+    }
+    
+    public void subirSpeed(){
+        
+    }
+    
+}
