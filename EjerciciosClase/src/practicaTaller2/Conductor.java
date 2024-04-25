138850bc5923518cb9c6198845008a2150030fd9
MRodzDirect - Original <mabarca1@utpl.edu.ec>
2024-04-24 20:40:46 -0500

    Ejercicio Vehiculos y chofer del Futuro Tasher ...

--- EjerciciosClase/src/practicaTaller2/Conductor.java
+++ EjerciciosClase/src/practicaTaller2/Conductor.java
@@ -1,6 +1,8 @@
 
 package practicaTaller2;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  *
  * @author USUARIO
@@ -11,7 +13,7 @@ public class Conductor {
     private String name;
     private String licenceType;
     private int age; // anio que nacio
-    private String licenceYear;
+    private int licenceYear;
     private String vehicle;
     private int multas;
     public Scanner in;
@@ -35,7 +37,7 @@ public class Conductor {
         return age;
     }
 
-    public String getLicenceYear() {
+    public int getLicenceYear() {
         return licenceYear;
     }
 
@@ -56,7 +58,7 @@ public class Conductor {
         this.age = age;
     }
 
-    public void setLicenceYear(String licenceYear) {
+    public void setLicenceYear(int licenceYear) {
         this.licenceYear = licenceYear;
     }
 
@@ -66,19 +68,89 @@ public class Conductor {
 
     
     public void validarEdad(){
-        if (age<18) {
+        if (this.getAge()<18) {
             System.out.println("Usted sera multado, es menor de edad");  
             multas++;
+        } else {
+            System.out.println("Usted es mayor de edad. No hay problemas.");
         }
     }
-    public void validarVehiculo(){
-        switch (vehiculo) {
+    public void validarTipoLicencia(String vehicle, String licenceType){
+        switch (vehicle.toLowerCase()) {
             case "moto":
-                this.licenceType="Tipo A";
+                if (!licenceType.equals("A")){
+                    System.out.println("Usted sera multado!.Por conducir un vehiculo no facultado por su tipo de Licencia!"); 
+                    multas++;
+                } else{
+                    System.out.println("Licencia Valida! :) . Buen chofer!");
+                }
+                
+                break;
+            case "carro":
+                if (!licenceType.equals("B")){
+                    System.out.println("Usted sera multado!.Por conducir un vehiculo no facultado por su tipo de Licencia!"); 
+                    multas++;
+                }  else{
+                    System.out.println("Licencia Valida! :) . Buen chofer!");
+                }
+                break;
+            case "camioneta":
+                if (!licenceType.equals("B")){
+                    System.out.println("Usted sera multado!.Por conducir un vehiculo no facultado por su tipo de Licencia!"); 
+                    multas++;
+                } else{
+                    System.out.println("Licencia Valida! :) . Buen chofer!");
+                }
+                break;
+            case "buseta":
+                if (!licenceType.equals("C")){
+                    System.out.println("Usted sera multado!.Por conducir un vehiculo no facultado por su tipo de Licencia!"); 
+                    multas++;
+                } else{
+                    System.out.println("Licencia Valida! :) . Buen chofer!");
+                }
+                break;
+            case "taxi":
+                if (!licenceType.equals("E")){
+                    System.out.println("Usted sera multado!.Por conducir un vehiculo no facultado por su tipo de Licencia!"); 
+                    multas++;
+                } else{
+                    System.out.println("Licencia Valida! :) . Buen chofer!");
+                }
                 break;
             default:
-                throw new AssertionError();
+                System.out.println("Tipo de Licencia falso. No mienta o sera multado!!!.");;
+                break; // What changing it to @continue@ will do ?
         }
     }
+
+    public void validarAñoLicencia() {
+        int currentYear=obtenerAñoActual(); // Supongamos que todas las licencias tienen una vigencia de 5 años a partir de su expedicion.
+        if (this.getLicenceYear()<currentYear-5){
+            System.out.println("Su licencia ha caducado. Usted la ha usado por mas de 5 años. Sera multado");
+            multas++;
+        } else {
+            System.out.println("Su licencia aun esta vigente. Felicitaciones!");
+                    
+        }
     }
+    
+    public int obtenerAñoActual(){
+        Date today = new Date();
+        SimpleDateFormat todayFormat = new SimpleDateFormat("dd/MM/Y");
+        String todayString = todayFormat.format(today);
+        
+        String today3 = todayString.replaceAll("\\d+{2}\\D+{2}","");
+        int currentYear = Integer.parseInt(today3);
+        return currentYear;
+       // RESPALDO, QUIZAS DEBA CAMBIAR EL PROGRAMA SEGUN LO DEMANDE EL PROFE. Sacamos edad en años de la persona
+       // System.out.println("Usted tiene: "+(currentYear-age)+" años");
+    }
+    public void showMultas(){ // Solo de ser necesario cambiaria este metodo por un getter. (Pues hasta aqui llega el programa y no hay necesidad de reutilizar el codigo de presentar las multas
+        System.out.println((multas==0)?"Usted tiene "+ multas+ " multas. Cielos Que macizo!":"Usted tiene "+ multas+ " multas.\nAcerquese a la ANT para que sepa cuanto debe de pagar. Jeje 😉");           
+    }// if ternariazo jiji.
+    }
+
+
+    
 
--- EjerciciosClase/src/practicaTaller2/Main.java
+++ EjerciciosClase/src/practicaTaller2/Main.java
@@ -1,7 +1,8 @@
 
 package practicaTaller2;
-
-
+/*
+author → El MRodzDirect 😉
+*/
 public class Main {
     
     public static void main(String[] args) {
@@ -10,12 +11,25 @@ public class Main {
             
     String name = chofer1.in.nextLine();
     String licenceType = chofer1.in.nextLine();
-    String age = chofer1.in.nextLine();
-    String licenceYear = chofer1.in.nextLine();
+    int age = chofer1.in.nextInt();
+    chofer1.setName(name);
+    chofer1.setLicenceType(licenceType);
+    chofer1.setAge(age);
+    // Validamos su edad de inmediato.
+    int licenceYear = chofer1.in.nextInt();
+    chofer1.setLicenceYear(licenceYear);
+    
+    chofer1.in.nextLine(); // Limpiamos el Buffer del Scanner
     System.out.println("Ingrese el vehiculo que usted tiene y le comunicaremos si esta sano de multas o debe pagar alguna.");
     String vehicle = chofer1.in.nextLine();
+    chofer1.setVehicle(vehicle);
     
-    }
- 
+    chofer1.validarEdad();
+    chofer1.validarTipoLicencia(vehicle,licenceType);
     
+    chofer1.validarAñoLicencia();
+    
+    chofer1.showMultas();
+    
+}
 }
